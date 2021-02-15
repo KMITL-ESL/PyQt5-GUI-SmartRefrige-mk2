7c530d911565f22a69f7c17aff8d628954fba45b
Sirawit <popeye-zz@hotmail.com>
2021-02-15 23:48:53 +0700

    basic Qt5

--- UserUI.py
+++ UserUI.py
@@ -2,11 +2,35 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+
+class MyWindow(QMainWindow): # inherit from QMainWindow
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 800, 500) # xpos, ypos, width, height
+        self.setWindowTitle("SMART REFRIGERATOR MK II")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.move(50,50)
+        self.label.setText("Welcome to Smart Refrige MK II")
+        self.update()
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 800, 500) # xpos, ypos, width, height
-    win.setWindowTitle("Smart Refrige MK 2")
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
